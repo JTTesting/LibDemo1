8c696b0cead6eeb023c60fad96159b968f1d0c9e
Jameson Turner <163557480+JamesonTM@users.noreply.github.com>
2025-04-24 11:28:02 -0500

    File updates

---
+++ LibDemo1/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace LibDemo1
+{
+    public class LibClass1
+    {
+        private string _message;
+        public LibClass1(string message) 
+        {
+            _message = message;
+        }
+        public string GetMessage()
+        {
+            return $"{_message} from Library1";
+        }
+    }
+}
